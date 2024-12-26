0561e6a4c3d87c1505c2bf3290923980f59b2102
Rakulan Sivathasan <s.rakulan04@gmail.com>
2024-12-26 23:23:06 +0100

    add prompt

--- src/main.c
+++ src/main.c
@@ -4,10 +4,29 @@
 #include <stdbool.h>
 #include <string.h>
 
+#define MAX_COMMAND_LEN 256
 
 int main(int argc, char* argv[], char* envp[]){
   
-  repl();
-  return 0;
+	char command[MAX_COMMAND_LEN];
+
+	printf("Bienvenue dans votre SGBD Rakus !\nPour sortir taper exit.\n");
+	while(1) {
+	
+		printf("> ");
+		if (fgets(command, MAX_COMMAND_LEN, stdin) == NULL) break;
+		command[strcspn(command, "\n")] = 0;
+
+		if (strcmp(command, "exit") == 0) {
+			printf("Au revoir !\n");
+			break;
+		}
+
+		printf("%s\n", command);
+	
+	}
+
+	//repl();
+	return 0;
   
 }
