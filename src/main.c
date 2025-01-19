ed6aa6d4c117312dd9645c566947dbf4f441a894
Rakulan Sivathasan <s.rakulan04@gmail.com>
2025-01-19 19:43:48 +0100

    reset to start

--- src/main.c
+++ src/main.c
@@ -7,26 +7,8 @@
 #define MAX_COMMAND_LEN 256
 
 int main(int argc, char* argv[], char* envp[]){
-  
-	char command[MAX_COMMAND_LEN];
 
-	printf("Bienvenue dans votre SGBD Rakus !\nPour sortir taper exit.\n");
-	while(1) {
-	
-		printf("> ");
-		if (fgets(command, MAX_COMMAND_LEN, stdin) == NULL) break;
-		command[strcspn(command, "\n")] = 0;
-
-		if (strcmp(command, "exit") == 0) {
-			printf("Au revoir !\n");
-			break;
-		}
-
-		printf("%s\n", command);
-	
-	}
-
-	//repl();
+	repl();
 	return 0;
-  
+
 }
