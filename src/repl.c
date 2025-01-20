775acc0874059dfd52fd0bcf8f25e9652d653c7e
Rakulan Sivathasan <s.rakulan04@gmail.com>
2025-01-20 15:38:01 +0100

    add insert statement

--- src/repl.c
+++ src/repl.c
@@ -72,20 +72,26 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
   }
 }
 
-PrepareResult prepare_statement(InputBuffer* input_buffer,
-                                Statement* statement) {
-
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
   if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
-    statement->type = STATEMENT_INSERT;
-    return PREPARE_SUCCESS;
+      statement->type = STATEMENT_INSERT;
+      int args_assigned = sscanf(
+          input_buffer->buffer, "insert %d %31s %254s",
+          &(statement->row_to_insert.id),
+          statement->row_to_insert.name,
+          statement->row_to_insert.email);
+      if (args_assigned < 3) {
+          return PREPARE_SYNTAX_ERROR;
+      }
+      return PREPARE_SUCCESS;
   }
   if (strcmp(input_buffer->buffer, "select") == 0) {
-    statement->type = STATEMENT_SELECT;
-    return PREPARE_SUCCESS;
+      statement->type = STATEMENT_SELECT;
+      return PREPARE_SUCCESS;
   }
-
   return PREPARE_UNRECOGNIZED_STATEMENT;
 }
+
 
 void execute_statement(Statement* statement) {
   switch (statement->type) {
